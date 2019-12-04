cd0c501b1b83b089072028ac83c09bfa49d6e48b
Boss <Boss@192.168.0.5>
2019-12-04 23:03:06 +0530

    second commit in branch1

--- Test/src/test/NewClass.java
+++ Test/src/test/NewClass.java
@@ -1,16 +1,10 @@
 package test;
 
 public class NewClass {
- Branch1
+
 private void syso() {
 	// TODO Auto-generated method stub
 System.out.println("Hai Nikhil");
 }
-=======
 
-	private void syso() {
-		// TODO Auto-generated method stub
-System.out.println("Hai Nikhil");
-	}
-master
 }
